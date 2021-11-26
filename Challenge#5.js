709df9b3ee18d8350e1ee92f4aed250aff2488a4
Maninder Sidhu <90073393+github4maninder@users.noreply.github.com>
2021-11-26 01:22:59 -0500

    Challenge #5 -You get a job, she gets a job, he gets a job
    Instructions
    
    Click here to learn how to navigate the code editor
    
    Now that a crew has been selected, you need to write a new function that will assign a job to each astronaut. Since you’ve had some experience with updating object properties in some of the previous challenges, this should be fairly straightforward. Just keep in mind that you need to return the astronaut after updating the job property.

---
+++ Challenge#5.js
@@ -0,0 +1,30 @@
+const addJobToAstronaut = (astronaut, job) => {
+  // Code here!
+
+  // Remember to return a value!
+  astronaut.job = job;
+  return astronaut;
+}
+
+// Examples
+
+// Input:
+
+
+//     const exampleAstronaut = {
+//       firstName:"Chris",
+//       lastName: "Hadfield",
+//       nickname:"Space Oddity",
+//       prefix:"Astronaut"
+//     }
+    
+// Output:
+
+
+//     const exampleAstronaut = {
+//       firstName:"Chris",
+//       lastName: "Hadfield",
+//       nickname:"Space Oddity",
+//       prefix:"Astronaut",
+//       job:"Shuttle DJ"
+//     }
