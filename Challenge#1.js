e441f9135eb2ce88f071e1fac7e0639e3e6a7495
Maninder Sidhu <90073393+github4maninder@users.noreply.github.com>
2021-11-26 01:14:31 -0500

    Challenge #1 - To compilation and beyond!
    Instructions
    
    Click here to learn how to navigate the code editor
    
    Our latest testing indicates a malfunctioning component in our communications apparatus. For some reason, messages from mission control are not being correctly displayed on board the shuttle. While the message is being displayed on the shuttle’s terminal, the sender’s name is not displayed beside it.
    
    Your first task is to write a function that will take two string parameters and return a string containing the sender name and the message in the form specified in the instructions.

---
+++ Challenge#1.js
@@ -0,0 +1,21 @@
+const parseMessage = (origin, message) => {
+  // Code here!
+   const newMessage = (message).toString();
+  // Want to see what's going on inside your function? 
+  // You can use console.log() to debug and print variables to the console below.
+  return (`${origin}: ${newMessage}`);
+  // Remember to return a value!
+};
+
+// Examples
+
+// Input:
+
+
+//     parseMessage("Mission Control", "Hello there!")
+    
+// Output:
+
+
+//     Mission Control: Hello there!
+
