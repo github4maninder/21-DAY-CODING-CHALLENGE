85e42e475cd5f60dc5cfe30a6943ea93360cb7f3
Maninder Sidhu <90073393+github4maninder@users.noreply.github.com>
2021-11-26 01:19:57 -0500

    Challenge #4 - Hope you’re not afraid of the dark
    Instructions
    
    Click here to learn how to navigate the code editor
    
    Opposites hm? In our daily lives, an opposite can mean multiple things, but in programming, it's different. Opposites can only be used when dealing with logical expressions or booleans. Knowing that the property inside our toggle is called isOn, we can infer that the value attached to it is either true, or false.
    
    Create a function that takes in a toggle object and will change the value of the property isOn between true and false and return the updated object. Using the function twice should revert the toggle back to its original value.

---
+++ Challenge#4.js
@@ -0,0 +1,43 @@
+const switchToggle = (toggle) => {
+  // Code here!
+
+  // Remember to return a value!
+  toggle.isOn = !toggle.isOn;
+  return toggle;
+}
+
+// Examples
+
+// Input:
+
+
+//     const someToggle = {
+//       name:"toggleA",
+//       isOn:false
+//     }
+    
+// Output:
+
+
+//     const someToggle = {
+//       name:"toggleA",
+//       isOn:true
+//     }
+    
+// Input:
+
+
+//     // If ran twice
+//     const someToggle = {
+//       name:"toggleB",
+//       isOn:true
+//     }
+    
+// Output:
+
+
+//     const someToggle = {
+//       name:"toggleB",
+//       isOn:true
+//     }
+    
