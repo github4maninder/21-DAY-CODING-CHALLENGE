48050370a408e5d06737b6829dfc0ea896dcb9e9
Maninder Sidhu <90073393+github4maninder@users.noreply.github.com>
2021-11-27 23:28:09 -0500

    Another day, another challenge
    Congratulating you on a job well done on the last task, your supervisor wastes no time in assigning you another one. This time, you’ll need to assign specific astronauts to a roster (list of astronauts).
    
    Essentially, the function needs to do two things: add an astronaut to the roster and return the updated array. As you exit your supervisor’s office and continue the linoleum-lined trek to that increasingly familiar storage closet, you feel a mixture of dread and curiosity as you wonder who, or what awaits you behind that pale white door.

---
+++ Challenge#6.js
@@ -0,0 +1,30 @@
+const addAstronautToRoster = (roster, astronaut) => {
+  // Code here!
+  roster.push(astronaut);
+  return roster;
+}
+
+// Examples
+
+// Input:
+
+
+//     const exampleRoster = []
+//     const exampleAstronaut = {
+//       firstName:"Chris",
+//       lastName: "Hadfield",
+//       nickname:"Space Oddity",
+//       prefix:"Astronaut"
+//     }
+    
+// Output:
+
+
+//     const exampleRoster = [
+//       {
+//         firstName:"Chris",
+//         lastName: "Hadfield",
+//         nickname:"Space Oddity",
+//         prefix:"Astronaut"
+//       }
+//     ]
